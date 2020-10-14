f34070595085b8353274c22cdd74591ca7c79ce9
yuzich <olga@yuzich.ru>
2020-10-14 14:37:07 +0700

    Убрала роутинг

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,52 @@
 <template>
   <div id="app">
-    <router-view></router-view>
+    <p>Мы показываем страницу {{ id }}</p>
+    <ul v-if="pages">
+    <page
+      :key="page.id"
+      v-for="page in pages"
+    ></page>
+    </ul>
+    <button v-on:click="id += 1">{{ button }}</button>
   </div>
 </template>
 
 <script>
-import axios from 'axios'
-export default {
-  name: 'app',
-  data () {
-    return {
-      msg: 'I like BEER',
-      button: 'Show next',
-      id: 1,
-      beers: [],
-      endpoint: 'https://api.punkapi.com/v2/'
-    }
-  },
+  import page from './components/page.vue';
+  import axios from "axios";
+  export default {
+    name: 'app',
+    components: {
+     page,
+    },
+    data () {
+      return {
+        msg: 'I like BEER',
+        button: 'Show next',
+        endpoint: 'https://api.punkapi.com/v2/beers?page=',
+        pages: [],
+        id: 1
+      }
+    },
 
-  created() {
-    this.getAllPosts();
-  },
+    methods: {
+      getPost(id) {
+        axios(this.endpoint + id)
+          .then(response => {
+            this.pages = response.data;
+            console.log(this.pages)
+          })
+          .catch( error => {
+            console.log('-----error-------');
+            console.log(error)
+          })
+      }
+    },
 
-  methods: {
-    getAllPosts() {
-      axios.get(this.endpoint)
-        .then(response => {
-          this.beers = response.data;
-          console.log(this.beers);
-        })
-        .catch(error => {
-          console.log('-----error-------');
-          console.log(error);
-        })
+    created() {
+      this.getPost(this.id);
     }
   }
-}
 </script>
 
 <style lang="scss">
---
+++ src/components/page.vue
@@ -0,0 +1,17 @@
+<template>
+
+    <ul v-if="pages">
+      <li>
+        <span>{{ page.id }}</span>
+        {{ page.name }}
+        <figure>
+          <img :src="page.image_url" :alt="page.name">
+        </figure>
+        <p>{{ page.description }}</p>
+        <p>{{ page.brewers_tips }}</p>
+      </li>
+    </ul>
+
+</template>
+
+
